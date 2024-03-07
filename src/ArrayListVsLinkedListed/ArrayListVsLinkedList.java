4096995425b077ad0e8fd8a8a3c4be0453cc52a9
kco <keremcanozcan35@gmail.com>
2024-03-07 19:31:34 +0300

    Array list versus linked list
    added first vs between array and linked lists

---
+++ src/ArrayListVsLinkedListed/ArrayListVsLinkedList.java
@@ -0,0 +1,42 @@
+package ArrayListVsLinkedListed;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayListVsLinkedList {
+    //ArrayList<Integer> arrayList = new ArrayList<>();
+    //        LinkedList<Integer> linkedList = new LinkedList<>();
+    //
+    //        long startTime;
+    //        long endTime;
+    //        long elapsedTime;
+    //
+    //        for(int i= 0; i < 1000000; i++){
+    //            arrayList.add(i);
+    //            linkedList.add(i);
+    //        }
+    //
+    //        startTime = System.nanoTime();
+    //        arrayList.get(50000);
+    //
+    //        endTime = System.nanoTime();
+    //
+    //        elapsedTime = endTime - startTime;
+    //
+    //        System.out.println("Array List: " + elapsedTime);
+    //
+    //        startTime = System.nanoTime();
+    //
+    //        linkedList.get(50000);
+    //
+    //        endTime = System.nanoTime();
+    //
+    //        elapsedTime = endTime - startTime;
+    //
+    //        System.out.println("Linked List: " + elapsedTime);
+
+
+
+}
